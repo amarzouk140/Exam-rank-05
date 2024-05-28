d1f713825cfcaddb85eb5e33a6ef078256142f83
Ayman <amarzouk@student.42abudhabi.ae>
2024-05-28 18:41:52 +0400

    q1

---
+++ cpp01_02/Dummy.hpp
@@ -0,0 +1,15 @@
+#ifndef DUMMY_HPP
+#define DUMMY_HPP
+
+#pragma once
+#include "ATarget.hpp"
+
+class Dummy : public ATarget
+{
+	public :
+		Dummy();
+		~Dummy();
+		ATarget* clone() const;
+};
+
+#endif
